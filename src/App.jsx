1deaabe5755b55463e8da21ae2b0451264aa14cd
Andrei Fabon <andrei17ig@gmail.com>
2025-04-21 20:13:21 -0600

    learning how to work with vite

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+    return (
+      <div>
+        <h1>Welcome to TD's Portfolio 🔥</h1>
+        <p>Let's get this Twitch integration rolling soon...</p>
+      </div>
+    );
+  }
