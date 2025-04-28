562339554c7268077f3619af8998944fe97284c7
Andrei Fabon <andrei17ig@gmail.com>
2025-04-28 11:52:07 -0600

    downlaoded react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from
 'react-router-dom';
-import './App.css';
 import Home from './pages/Home';
 import Twitch from './pages/Twitch';
 import About from './pages/About';
